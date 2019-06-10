b5ae46b715c641c63b07394a5311321d3be759ad
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2019-06-10 21:55:36 +0530

    build react

---
+++ build/precache-manifest.4ce8e95256d02880b5c92b8738835e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c22d1376905be46d9e4d7010145a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1260938bdb3b6ecb6b71",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9ba928a739b832c9726f",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1260938bdb3b6ecb6b71",
+    "url": "/static/js/2.9743da89.chunk.js"
+  },
+  {
+    "revision": "9ba928a739b832c9726f",
+    "url": "/static/js/main.c302435f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
