6abba1dbbfde9760a20265f0849f3576451de670
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2019-06-10 21:42:34 +0530

    react + firebase init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from "firebase";
 import * as serviceWorker from './serviceWorker';
+
+let config = {
+    apiKey: "AIzaSyA_VRTk3r99wP7TcLqAjqk9Cl76CgavSj0",
+    authDomain: "bscps1.firebaseapp.com",
+    databaseURL: "https://bscps1.firebaseio.com",
+    projectId: "bscps1",
+    storageBucket: "bscps1.appspot.com",
+    messagingSenderId: "741658268768",
+    appId: "1:741658268768:web:c6593e3a6293f4d1"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
