a4fb89619ad8ee63e71a5d1bfd143e659997827c
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2019-06-11 16:34:32 +0530

    setup mic and waves with listen btn

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 // import logo from './logo.svg';
 //TODO: change the manifest.json to include BITS logo of all sizes
 import './App.css';
-
+import {Button} from "reactstrap";
 function App() {
   return (
     <div className="App">
-      <div className="container d-flex justify-content-center align-items-center">
-        <iframe
-          title="chatbot"
-          allow="microphone;"
-          width="350"
-          height="550"
-          src="https://console.dialogflow.com/api-client/demo/embedded/be9403d8-f844-41c9-9bc8-1c07089ad966">
-        </iframe>
+      <div className="container">
+        <div className="row">
+          <div className="wrapper">
+            <img src={window.location.origin+"/microphone.svg"} alt="mic" id="mic"/>
+            <img src={window.location.origin+"/radio-waves.svg"} alt="waves" id="waves"/>
+          </div>
+        </div>
+        <div className="row">
+          <Button onClick={ (e) => {
+            e.preventDefault();
+          }} color="success">Listen</Button>
+        </div>
       </div>
     </div>
   );
